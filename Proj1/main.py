adf184362a2fec87f8a55008f2c3a2602222d8e5
Joao Victor <joao.victors1203@gmail.com>
2019-09-04 15:12:23 -0300

    pequenas mudancas

--- Proj1/main.py
+++ Proj1/main.py
@@ -1,4 +1,9 @@
-import socket, ssl, json, time, random, tkinter
+import socket
+import ssl
+import json
+import tkinter
+import threading
+
 
 class app(tkinter.Tk):
 
@@ -6,38 +11,60 @@ class app(tkinter.Tk):
         tkinter.Tk.__init__(self, parent)
         self.parent = parent
         self.initialize()
-    
+
     def initialize(self):
+        self.resizable(0, 0)
         self.grid()
-        self.minsize(350,100)
-        
-        self.label_Link = tkinter.Label(self, text = "Insert video URL:") # label
-        self.label_Link.grid(column = 0, row = 0, padx = 10, pady = (10,5))
+        self.minsize(350, 100)
 
-        self.container = tkinter.Frame(self) # input + button frame
-        self.container.grid(column = 0, row = 1, padx = 8, pady = 5)
+        self.label_Link = tkinter.Label(
+            self, text="Insert video URL:")  # label
+        self.label_Link.grid(column=0, row=0, padx=10, pady=(10, 5))
 
-        self.entry_Link = tkinter.Entry(self.container,width=50) # input
+        self.container = tkinter.Frame(self)  # input + button frame
+        self.container.grid(column=0, row=1, padx=8, pady=5)
+
+        self.entry_Link = tkinter.Entry(self.container, width=50)  # input
         #self.entry_Link.grid(column = 0, row = 1, padx = (10,3), pady = 5)
-        self.entry_Link.pack(side="left", padx = 5)
-        
-        self.button_Search = tkinter.Button(self.container, text = "Search",
-                                            command = self.Search) # button
+        self.entry_Link.pack(side="left", padx=5)
+        self.bind("<Return>", (lambda event: self.SearchCall()))
+
+        self.button_Search = tkinter.Button(self.container, text="Search", command=self.SearchCall)  # button
         #self.button_Search.grid(column = 1, row = 1, padx = (0,10))
-        self.button_Search.pack(side="right", padx = 1)
-        
-        self.entry_Views = tkinter.Label(self, text = "") # response
-        self.entry_Views.grid(column = 0, row = 2, pady = 1)
+        self.button_Search.pack(side="right", padx=1)
+
+        self.label_Views = tkinter.Label(self, text="")  # response
+        self.label_Views.grid(column=0, row=2, pady=3)
+
+    def SearchCall(self):
+        t = threading.Thread(target=self.Search)
+        t.start()
+        self.label_Views["text"] = "Loading..."
 
     def Search(self):
         try:
+            yt_valid = [
+                "youtube.com/watch?v",
+                "http://youtube.com/watch?v",
+                "http://www.youtube.com/watch?v",
+                "www.youtube.com/watch?v",
+                "https://youtube.com/watch?v",
+                "https://www.youtube.com/watch?v"
+            ]
             link_video = str(self.entry_Link.get()).split("=")
+
+            if(link_video[0] not in yt_valid):
+                raise(IndexError("invalid"))
+            
             id_video = link_video[1].split("&")[0]
 
-            self.entry_Views["text"] = self.return_data(self.get_data(id_video))
+            if(id_video == ""):
+                raise(IndexError("invalid"))
+
+            self.label_Views["text"] = self.return_data(self.get_data(id_video))
         except IndexError:
-            self.entry_Views["text"] = "Invalid URL. Please provide a full YouTube URL:\nhttps://www.youtube.com/watch?v=xxxxxxxx"
-    
+            self.label_Views["text"] = "Invalid URL. Please provide a full YouTube URL:\nhttps://www.youtube.com/watch?v=xxxxxxxx"
+
     def get_data(self, id_video):
         try:
             hostname = "www.googleapis.com"
@@ -49,24 +76,24 @@ class app(tkinter.Tk):
             request += b"HTTP/1.1\r\n"
             request += b"Host: www.googleapis.com\r\n"
             request += b"Accept: application/json\r\n\r\n"
-            
+
             timeout = 5
 
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
             sock.settimeout(timeout)
 
-            ssock = ssl.wrap_socket(sock, server_side = False, ssl_version=ssl.PROTOCOL_TLS)
+            ssock = ssl.wrap_socket(sock, server_side=False, ssl_version=ssl.PROTOCOL_TLS)
             ssock.connect(addr)
             ssock.sendall(request)
 
-            str_headers = ssock.recv() # headers
+            str_headers = ssock.recv()  # headers
             str_json = ""
             while True:
                 try:
                     str_json += ssock.recv().decode("utf-8")
                 except socket.timeout:
                     break
-                
+
             ssock.close()
             sock.close()
             return str_json
@@ -85,7 +112,7 @@ class app(tkinter.Tk):
             except ValueError:
                 return "An error has occured. Please try again."
             else:
-                return "\"" + titulo_video + "\" has " + qtd_with_comma.replace(",",".") + " views"
+                return "\"" + titulo_video + "\" has " + qtd_with_comma.replace(",", ".") + " views"
         else:
             return "An error has occured. Please try again."
 

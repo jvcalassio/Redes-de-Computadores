69b0c5c69701807d808d9d243d35c1ca1904efce
jvcalassio <joao.victors1203@gmail.com>
2019-10-31 10:44:57 -0300

    proj2 funcional

--- Proj2/client.py
+++ Proj2/client.py
@@ -6,15 +6,114 @@ import time
 host = ("localhost", 8091)
 vazao = 8000 # bps
 distancia = 10 # metros
-proberro = 0 # 0-100%
+proberro = 10 # 0-100%
 timeout = 1
+pktsize = 20 # tamanho do pacote
+mspeed = 2 * 10**8 # velocidade do meio (fibra otica)
+janela = 7
+atraso = (pktsize/vazao) + (distancia/mspeed)
 
-class main():
+##################################################
+# Atraso de transmissao (Dtrans) = L / R =>
+#   quantidade de tempo pro router "empurrar" o pa
+#   cote, nada a ver com distancia fisica dos routers
+# L = tamanho do pacote (bits)
+# R = banda do enlace (bps)
+#
+# Atraso de propagacao (Dprop) = S / v =>
+#   tempo que 1 bit leva pra se propagar de um router
+#   a outro
+# S = distancia do enlace (metros)
+# v = velocidade do meio de propagacao (~2x10^8 m/s)
+#
+# Atraso nodal (Dnodal) = Dtrans + Dprop
+##################################################
+
+
+##################################################
+# Calc atraso =
+# atraso = ((pktsize)/vazao) + (distancia/2*10^8)
+##################################################
+
+class srepeat():
     def __init__(self):
         self.initialize()
 
     def initialize(self):
-        conn = connection()
+        conn = connection_sr()
+        nextwindow = [0] * janela # janela a ser enviada
+        timeouts = [0] * janela # timeout de cada pacote da janela
+        ackedpackets = [False] * janela # pacotes ja ACK
+        for index in range(0,janela):
+            nextwindow[index] = random.randint(0,255)
+            timeouts[index] = time.time() + timeout
+
+        conn.send_window(nextwindow, timeouts, ackedpackets) # envia a primeira janela
+        while True:
+            #time.sleep(timeout)
+            time.sleep(1)
+            conn.recv_msg(ackedpackets)
+
+            isAllAcked = True
+            for i,isAcked in enumerate(ackedpackets):
+                if(not isAcked):
+                    isAllAcked = False
+                    if(timeouts[i] + 2 < time.time()): # se ja passou o timeout do pacote, reenvia
+                        conn.send_individual_packet(i, nextwindow[i], timeouts, ackedpackets)
+
+            if(isAllAcked): # se todos ja foram enviados, muda janela
+                for index in range(0,janela):
+                    nextwindow[index] = random.randint(0,255)
+                    timeouts[index] = time.time() + timeout
+                    ackedpackets[index] = False
+                    
+                conn.send_window(nextwindow, timeouts, ackedpackets)
+
+
+class connection_sr():
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        timeout = (atraso * 2)
+        self.sock.settimeout(timeout)
+
+    def send_window(self, nextwindow, timeouts, ackedpackets): # enviar os N pacotes da janela
+        for index,packet in enumerate(nextwindow):
+            failsend = random.randint(0,100)
+            if (failsend >= proberro): # se n der erro, envia
+                tosend = str(index) + " " + str(packet) # envia "X MSG"
+                self.sock.sendto(tosend.encode(), host)
+                timeouts[index] = time.time() + timeout
+                ackedpackets[index] = False
+                print("Pacote #" + str(index) + " (" + str(packet) + ") enviado")
+
+    def send_individual_packet(self, i, packet, timeouts, ackedpackets):
+        failsend = random.randint(0,100)
+        if (failsend >= proberro):
+            tosend = str(i) + " " + str(packet)
+            self.sock.sendto(tosend.encode(), host)
+            timeouts[i] = time.time() + timeout
+            ackedpackets[i] = False
+            print("Pacote #" + str(i) + " (" + str(packet) + ") reenviado")
+
+    def recv_msg(self, ackedpackets): # retorna mensagem
+        try:
+            msg, server = self.sock.recvfrom(pktsize)
+            print("Recebido " + msg.decode())
+            arr_msg = msg.decode().split(" ") # recebe X ACK
+            
+            identifier = int(arr_msg[0])
+            ackedpackets[identifier] = True
+            return
+        except socket.timeout:
+            return
+
+
+class stopnwait():
+    def __init__(self):
+        self.initialize()
+
+    def initialize(self):
+        conn = connection_snw()
         msg = str(random.randint(0,255))
         last_sent = False # ultima mensagem enviada tem identificador 0, inicialmente
         conn.send_msg(msg, last_sent)
@@ -22,29 +121,33 @@ class main():
         while True:
             time.sleep(timeout)
             (identifier, rmsg) = conn.recv_msg()
-            if rmsg == None or identifier == last_sent:
+            if rmsg == None or identifier == last_sent: # se nao receber a msg (timeout) ou se o ACK nao bater com o da msg
                 conn.send_msg(msg, last_sent)
             else:
                 msg = str(random.randint(0,255))
                 last_sent = not last_sent
                 conn.send_msg(msg, last_sent)
 
-class connection():
+class connection_snw():
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.settimeout(2)
-        print("Conectado")
+        timeout = (2 * atraso)
+        self.sock.settimeout(timeout)
 
     def send_msg(self, msg, ls):
         msg = ("1" if ls else "0") + " " + msg
-        self.sock.sendto(msg.encode(), host)
+        failsend = random.randint(0,100)
         print("#" + msg + " enviado")
+        if(failsend > proberro): # se nao houver erro na transmissao da mensagem
+            self.sock.sendto(msg.encode(), host)
+        else:
+            print("ocorreu um erro na transmissao")
 
     def recv_msg(self):
         try:
-            msg, server = self.sock.recvfrom(5)
+            msg, server = self.sock.recvfrom(pktsize)
             recvmsg = msg.decode().split(" ")
-            print(recvmsg[1] + " recebido")
+            print("#" + recvmsg[0] + " " + recvmsg[1] + " recebido")
             return (recvmsg[0], recvmsg[1])
         except socket.timeout:
             return (None, None)
@@ -52,4 +155,4 @@ class connection():
     def close(self):
         self.sock.close()
 
-mapp = main()
+mapp = srepeat()
--- Proj2/server.py
+++ Proj2/server.py
@@ -1,28 +1,93 @@
 import socket
+import random
+import time
 
 host = ("localhost", 8091)
 
-class main():
+vazao = 8000 # bps
+distancia = 10 # metros
+proberro = 10 # 0-100%
+timeout = 1
+pktsize = 17 # tamanho do pacote
+mspeed = 2 * 10**8 # velocidade do meio (fibra otica)
+janela = 7
+
+class srepeat():
     def __init__(self):
-        self.initialize()
+        sv = server_sr()
 
-    def initialize(self):
-        sv = server()
-
-class server():
+class server_sr():
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        atraso = ((pktsize)/vazao) + (distancia/mspeed)
+        self.sock.settimeout(atraso)
         self.sock.bind(host)
+        print("Esperando mensagens")
+        self.listen()
+
+    def listen(self):
+        buffer = [None] * janela
+        delivered = []
+        while True:
+            try:
+                time.sleep(1)
+                msg, cliente = self.sock.recvfrom(pktsize)
+                print("Mensagem recebida: ", msg.decode())
+                arr_msg = msg.decode().split(" ") # arr_msg[0] = codigo; 
+                                                  # arr_msg[1] = msg;
+                
+                identifier = int(arr_msg[0])
+                buffer[identifier] = arr_msg[1] # salva o pacote no buffer
+                # envia ACK do pacote recebido
+                failsend = random.randint(0,100)
+
+                if(failsend >= proberro): # se nao cumprir a prob de erro
+                    self.sock.sendto((arr_msg[0] + " ACK").encode(), cliente) # envia X ACK
+                    print("ACK #" + arr_msg[0] + " enviado")
+                else:
+                    print("Erro ao enviar o ACK #" + arr_msg[0])
+
+                complete = True
+                for i,data in enumerate(buffer):
+                    if(data == None):
+                        complete = False
+
+                if(complete): # janela completa, salva no delivered
+                    delivered.append(buffer)
+                    for i in range(0,janela-1):
+                        buffer[i] = None
+
+            except socket.timeout:
+                continue
+
+class stopnwait():
+    def __init__(self):
+        sv = server_snw()
+
+class server_snw():
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        atraso = ((pktsize)/vazao) + (distancia/mspeed)
+        self.sock.settimeout(atraso)
+        self.sock.bind(host)
+        print("Esperando mensagens")
         self.listen()
 
     def listen(self):
         while True:
-            msg, cliente = self.sock.recvfrom(17)
-            print("Mensagem recebida: " + msg.decode())
-            identifier = msg.decode().split(" ")[0]
-            self.sock.sendto((identifier + " ACK").encode(), cliente)
+            try:
+                msg, cliente = self.sock.recvfrom(pktsize)
+                print("Mensagem recebida: " + msg.decode())
+                identifier = msg.decode().split(" ")[0]
+                failsend = random.randint(0,100)
+                if(failsend > proberro): # se nao houver erro na transmissao do ack
+                    self.sock.sendto((identifier + " ACK").encode(), cliente)
+                else:
+                    print("erro na transmissao do ACK " + identifier)
+            except:
+                continue
 
     def close(self):
         self.sock.close()
 
-m = main()
+m = srepeat()
